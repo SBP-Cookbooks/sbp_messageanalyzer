8e21f0420967c68b481f7230ba48838bca2f53fd
Stephen Hoekstra <shoekstra@schubergphilis.com>
2017-04-22 14:28:38 +0200

    Release 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@schubergphilis.com'
 license          'Apache 2.0'
 description      'Installs Microsoft Message Analyser.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.2.0'
 source_url       'https://github.com/sbp-cookbooks/sbp_messageanalyzer'
 issues_url       'https://github.com/sbp-cookbooks/sbp_messageanalyzer/issues'
 
